c96ce12a8a388ba9b65a26bfd5ad4120f88d04a6
OC2427 <om.chandrakar2427@gmail.com>
2026-08-05 23:45:36 +0530

    Add files via upload
    Electronic Configuration of Atoms

---
+++ elementname.py
@@ -0,0 +1,270 @@
+def at_no(string):
+    num=0
+    if string.lower()=='hydrogen':
+        num=1
+    elif string.lower()=='helium':
+        num=2
+    elif string.lower()=='lithium':
+        num=3
+    elif string.lower()=='berylium':
+        num=4
+    elif string.lower()=='boron':
+        num=5
+    elif string.lower()=='carbon':
+        num=6
+    elif string.lower()=='nitrogen':
+        num=7
+    elif string.lower()=='oxygen':
+        num=8
+    elif string.lower()=='fluorine':
+        num=9
+    elif string.lower()=='neon':
+        num=10
+    elif string.lower()=='sodium':
+        num=11
+    elif string.lower()=='magnesium':
+        num=12
+    elif string.lower()=='aluminium':
+        num=13
+    elif string.lower()=='silicon':
+        num=14
+    elif string.lower()=='phosphorous':
+        num=15
+    elif string.lower()=='sulphur':
+        num=16
+    elif string.lower()=='chlorine':
+        num=17
+    elif string.lower()=='argon':
+        num=18
+    elif string.lower()=='potassium':
+        num=19
+    elif string.lower()=='calcium':
+        num=20
+    elif string.lower()=='scandium':
+        num=21
+    elif string.lower()=='titanium':
+        num=22
+    elif string.lower()=='vanadium':
+        num=23
+    elif string.lower()=='chromium':
+        num=24
+    elif string.lower()=='manganese':
+        num=25
+    elif string.lower()=='iron':
+        num=26
+    elif string.lower()=='cobalt':
+        num=27
+    elif string.lower()=='nickel':
+        num=28
+    elif string.lower()=='copper':
+        num=29
+    elif string.lower()=='zinc':
+        num=30
+    elif string.lower()=='gallium':
+        num=31
+    elif string.lower()=='germanium':
+        num=32
+    elif string.lower()=='arsenic':
+        num=33
+    elif string.lower()=='selenium':
+        num=34
+    elif string.lower()=='bromine':
+        num=35
+    elif string.lower()=='krypton':
+        num=36
+    elif string.lower()=='rubidium':
+        num=37
+    elif string.lower()=='strontium':
+        num=38
+    elif string.lower()=='yttrium':
+        num=39
+    elif string.lower()=='zirconium':
+        num=40
+    elif string.lower()=='niobium':
+        num=41
+    elif string.lower()=='molybdenum':
+        num=42
+    elif string.lower()=='technetum':
+        num=43
+    elif string.lower()=='ruthenium':
+        num=44
+    elif string.lower()=='rhodium':
+        num=45
+    elif string.lower()=='palladium':
+        num=46
+    elif string.lower()=='silver':
+        num=47
+    elif string.lower()=='cadmium':
+        num=48
+    elif string.lower()=='indium':
+        num=49
+    elif string.lower()=='tin':
+        num=50
+    elif string.lower()=='antimony':
+        num=51
+    elif string.lower()=='tellurium':
+        num=52
+    elif string.lower()=='iodine':
+        num=53
+    elif string.lower()=='xenon':
+        num=54
+    elif string.lower()=='cesium':
+        num=55
+    elif string.lower()=='barium':
+        num=56
+    elif string.lower()=='lanthanum':
+        num=57
+    elif string.lower()=='cerium':
+        num=58
+    elif string.lower()=='praeodynium':
+        num=59
+    elif string.lower()=='neodymium':
+        num=60
+    elif string.lower()=='promethium':
+        num=61
+    elif string.lower()=='samarium':
+        num=62
+    elif string.lower()=='europium':
+        num=63
+    elif string.lower()=='gadolinium':
+        num=64
+    elif string.lower()=='terbium':
+        num=65
+    elif string.lower()=='dysprosium':
+        num=66
+    elif string.lower()=='holmium':
+        num=67
+    elif string.lower()=='erbium':
+        num=68
+    elif string.lower()=='thulium':
+        num=69
+    elif string.lower()=='ytterbium':
+        num=70
+    elif string.lower()=='lutetium':
+        num=71
+    elif string.lower()=='hafnium':
+        num=72
+    elif string.lower()=='tantalum':
+        num=73
+    elif string.lower()=='tungsten':
+        num=74
+    elif string.lower()=='rhenium':
+        num=75
+    elif string.lower()=='osmium':
+        num=76
+    elif string.lower()=='iridium':
+        num=77
+    elif string.lower()=='platinum':
+        num=78
+    elif string.lower()=='gold':
+        num=79
+    elif string.lower()=='mercury':
+        num=80
+    elif string.lower()=='thallium':
+        num=81
+    elif string.lower()=='lead':
+        num=82
+    elif string.lower()=='bismuth':
+        num=83
+    elif string.lower()=='polonium':
+        num=84
+    elif string.lower()=='astatine':
+        num=85
+    elif string.lower()=='radon':
+        num=86
+    elif string.lower()=='francium':
+        num=87
+    elif string.lower()=='radium':
+        num=88
+    elif string.lower()=='actinium':
+        num=89
+    elif string.lower()=='thorium':
+        num=90
+    elif string.lower()=='protactium':
+        num=91
+    elif string.lower()=='uranium':
+        num=92
+    elif string.lower()=='neptunium':
+        num=93
+    elif string.lower()=='plutonium':
+        num=94
+    elif string.lower()=='americium':
+        num=95
+    elif string.lower()=='curium':
+        num=96
+    elif string.lower()=='berkelium':
+        num=97
+    elif string.lower()=='californium':
+        num=98
+    elif string.lower()=='einsteinium':
+        num=99
+    elif string.lower()=='fermium':
+        num=100
+    elif string.lower()=='mendelevim':
+        num=101
+    elif string.lower()=='nobelium':
+        num=102
+    elif string.lower()=='lawrencium':
+        num=103
+    elif string.lower()=='rutherfordium':
+        num=104
+    elif string.lower()=='dubnium':
+        num=105
+    elif string.lower()=='seaborgium':
+        num=106
+    elif string.lower()=='bohrium':
+        num=107
+    elif string.lower()=='hassium':
+        num=108
+    elif string.lower()=='meitnerium':
+        num=109
+    elif string.lower()=='darmstadtium':
+        num=110
+    elif string.lower()=='roentgenium':
+        num=111
+    elif string.lower()=='copernicium':
+        num=112
+    elif string.lower()=='nihonium':
+        num=113
+    elif string.lower()=='flerovium':
+        num=114
+    elif string.lower()=='moscovium':
+        num=115
+    elif string.lower()=='livermorium':
+        num=116
+    elif string.lower()=='tenessine':
+        num=117
+    elif string.lower()=='ognesson':
+        num=118
+    else:
+        num=-1
+    return num
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
